5c768cc2a0c19d5cd703f8706cfce9a2fc4fcb2b
Dilpreet-singh-Hardoi <146751980+Dilpreet-singh-Hardoi@users.noreply.github.com>
2026-02-01 17:30:09 -0500

    Added User entity with Lombok annotations

---
+++ src/main/java/com/example/cleanarchitecture/domain/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.cleanarchitecture.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+}
+
