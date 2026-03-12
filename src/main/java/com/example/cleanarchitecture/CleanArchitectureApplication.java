e2d0eb680ee056c0a3dcae8a2e7f75f9afae21bf
Dilpreet-singh-Hardoi <146751980+Dilpreet-singh-Hardoi@users.noreply.github.com>
2026-03-12 16:37:30 -0400

    Added Swagger API documentation

--- src/main/java/com/example/cleanarchitecture/CleanArchitectureApplication.java
+++ src/main/java/com/example/cleanarchitecture/CleanArchitectureApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CleanarchitectureApplication {
+public class CleanArchitectureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CleanarchitectureApplication.class, args);
+		SpringApplication.run(CleanArchitectureApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/cleanarchitecture/CleanArchitectureApplicationTests.java
+++ src/test/java/com/example/cleanarchitecture/CleanArchitectureApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CleanarchitectureApplicationTests {
+class CleanArchitectureApplicationTests {
 
 	@Test
 	void contextLoads() {
